2cca7a61fc5fdfba5ea870ce476736b235c171fc
Talha991s <55731371+Talha991s@users.noreply.github.com>
2020-12-17 00:40:35 -0800

    Initial Commit

---
+++ Assets/Characters/Animation/CharacterWalkAnimController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CardinalDirection
+{
+    NORTH   = 0,
+    EAST    = 1,
+    SOUTH   = 2,
+    WEST    = 3
+}
+
+
+public class CharacterWalkAnimController : MonoBehaviour
+{
+    [SerializeField]
+    Animator animator;
+
+    [SerializeField]
+    Rigidbody2D rb;
+
+    [SerializeField]
+    CardinalDirection walkDirection = CardinalDirection.SOUTH;
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+       //Vector2 vel = rb.velocity;
+       Vector2 vel = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        // if we are moving at all
+        bool isWalking = vel.magnitude > float.Epsilon;
+
+       if (isWalking)
+       {
+            //
+            bool isMovingHorizontally = Mathf.Abs(vel.x) > Mathf.Abs(vel.y);
+           if (isMovingHorizontally) //moving left/right more than up/down
+           {
+               if (vel.x < 0) //moving left/WEST
+               {
+                   walkDirection = CardinalDirection.WEST;
+               }
+               else //moving right/EAST
+               {
+                   walkDirection = CardinalDirection.EAST;
+               }
+           }
+           else // moving more vertically
+           {
+               if (vel.y < 0) //moving down/SOUTH
+               {
+                   walkDirection = CardinalDirection.SOUTH;
+               }
+               else //moving up/NORTH
+               {
+                   walkDirection = CardinalDirection.NORTH;
+               }
+           }
+       } // if is walking
+
+       animator.SetBool("IsWalking", isWalking);
+       animator.SetInteger("WalkDirection", (int)walkDirection);
+    }
+}
---
+++ Assets/Characters/Scripts/PlayerCharacterController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacterController : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 5;
+
+    [SerializeField]
+    Rigidbody2D rigidBody;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 movementVector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        movementVector *= speed;
+        rigidBody.velocity = movementVector;
+    }
+}
